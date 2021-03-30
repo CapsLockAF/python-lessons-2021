29509155a6e6397234085b32087b70eea7834d46
o.fareniuk <fareniuk.olexandr@gmail.com>
2021-03-30 16:13:32 +0300

    add [task2.py] iputs, prints

--- HW1/task2.py
+++ HW1/task2.py
@@ -1,4 +1,10 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+adress = input("Where are you live? ")
 
+print("Hello,", name)
+print("Your age is", age)
+print("You live in " + adress)
 
 
 
