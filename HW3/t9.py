e491c3714f2ddb5ab71075794f7787527c01f83f
o.fareniuk <fareniuk.olexandr@gmail.com>
2021-04-08 12:10:02 +0300

    fix(t9): condition prodlems

--- HW3/t9.py
+++ HW3/t9.py
@@ -9,13 +9,13 @@ banknote_10 = 0
 if amount >= 200:
     banknote_200 = int(amount / 200)
     amount %= 200
-if amount > 100:
+if amount >= 100:
     banknote_100 = int(amount / 100)
     amount %= 100
-if amount > 10:
+if amount >= 10:
     banknote_10 = int(amount / 10)
     amount %= 10
 
 print("200 UAH * %i \n 100 UAH * %i \n 10 UAH * %i \n 1 UAH * %i"
-      % (banknote_200, banknote_100, banknote_10, amount) if amount > 0
+      % (banknote_200, banknote_100, banknote_10, amount) if amount >= 0
       else "Enter positive number")
