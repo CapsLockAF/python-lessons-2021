087c2f71db939cb7d3f0e59b42898273c2bd85a3
o.fareniuk <fareniuk.olexandr@gmail.com>
2021-04-26 18:29:46 +0300

    fix(HW8/): in task 7 refactoring code

--- HW8__Functions/t6.py
+++ HW8__Functions/t6.py
@@ -2,16 +2,12 @@
 # і повертає True, якщо воно просте, і False - в іншому випадку
 
 def is_prime(x):
-    count = 0
     for j in range(2, x):
         if x % j == 0:
-            count += 1
-    if count == 0:
-        return True
-    else:
-        return False
+            return False
+    return True
 
 # 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61 ...
 
 
-print(is_prime(89))
+print(is_prime(12))
